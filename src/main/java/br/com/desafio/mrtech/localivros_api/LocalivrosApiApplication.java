378ec14ba37ef4f440e4cc6a2d9f36a6601358e4
Emanoel <emanoel.pluto@gmail.com>
2026-07-11 20:16:23 -0300

    first commit

---
+++ src/main/java/br/com/desafio/mrtech/localivros_api/LocalivrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.mrtech.localivros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalivrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalivrosApiApplication.class, args);
+	}
+
+}
